c501ab12debf48d7ade31c4597dc41d914e69511
Vinayak Jaiwant <vjaiwantx@gmail.com>
2022-10-15 09:22:05 +0530

    Added Scrollspy

--- viewer-frontend/src/app/main-view/main-view.component.ts
+++ viewer-frontend/src/app/main-view/main-view.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy,Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
-  styleUrls: ['./main-view.component.css']
+  styleUrls: ['./main-view.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainViewComponent implements OnInit {
-
+  items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
   constructor() { }
-  isOpen = false;
+
   ngOnInit(): void {
   }
 
