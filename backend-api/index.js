389bcf03f7753b0648323ddf825981ae7355f46e
Vinayak Jaiwant <vjaiwantx@gmail.com>
2022-10-15 16:37:49 +0530

    Database connection complete

--- backend-api/index.js
+++ backend-api/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 const port = 8080;
 const routes = require('./routes/route');
+const db = require('./knex/knex');
 
 
 app.use(express.json())
@@ -14,6 +15,20 @@ app.get('/', (req, res) => {
   res.json({"message": 'Fabrik 3D Model Supplier API'});
 })
 
+const testdb = async () => {
+    try 
+    {
+      const testKnexConnection = await db.raw("SELECT 1+1");
+    }
+    catch(err) 
+    {
+      console.log("Database Connection Error");
+      console.log(err);
+    }
+  }
+
+
 app.listen(port, () => {
+    testdb();
     console.log(`Server listening on port ${port}`)
 })
---
+++ backend-api/knex/knexfile.js
@@ -0,0 +1,25 @@
+
+
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+ module.exports = {
+    testing: {
+      client: 'pg',
+      connection: {
+        host: 'localhost',
+        database: 'models',
+        user: 'postgres',
+        password: ''
+      },
+      migrations: {
+        directory: __dirname + '/knex/migrations',
+      },
+      seeds : {
+        directory: __dirname + '/knex/seeds',
+      }
+    }
+  
+  };
