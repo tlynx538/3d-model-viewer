763a812fc7bc2cd8513a0bd9aef9b21218441db0
Vinayak Jaiwant <vjaiwantx@gmail.com>
2022-10-15 11:48:47 +0530

    Fixed routing

--- viewer-frontend/src/app/app-routing.module.ts
+++ viewer-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MainViewComponent } from './main-view/main-view.component';
+import { SidebarUploadComponent } from './sidebar-upload/sidebar-upload.component';
+const routes: Routes = [
+  {
+    path: '', component:MainViewComponent
+  },
+  {
+    path: 'upload', component:SidebarUploadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- viewer-frontend/src/app/main-view/main-view.component.ts
+++ viewer-frontend/src/app/main-view/main-view.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy,Component, OnInit } from '@angular/core';
-
+//import {MatDialog, MatDialogRef,DialogService} from '@angular/material/dialog';
+//import { SidebarUploadComponent } from '../sidebar-upload/sidebar-upload.component';
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -8,9 +9,8 @@ import { ChangeDetectionStrategy,Component, OnInit } from '@angular/core';
 })
 export class MainViewComponent implements OnInit {
   items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
-  constructor() { }
-
+  constructor() {
+   }
   ngOnInit(): void {
   }
-
 }
