54a99b3a5ab0df17ba0cbb4ef2509c7a796f1496
Vinayak Jaiwant <vjaiwantx@gmail.com>
2022-10-15 15:47:49 +0530

    Init

---
+++ backend-api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 8080;
+//const routes = require('./routes/route');
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:false }));
+app.use(morgan('combined'));
+
+//app.use('/api/',routes);
+app.get('/', (req, res) => {
+  res.json({"message": 'Fabrik 3D Model Supplier API'});
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
