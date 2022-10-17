e5d69cb10f5e11264d4588f1637788f5d659a041
Vinayak Jaiwant <vjaiwantx@gmail.com>
2022-10-17 08:39:25 +0530

    Get files (backend)

--- backend-api/controllers/controller.js
+++ backend-api/controllers/controller.js
@@ -1,5 +1,16 @@
 const uploadFile = require('../middleware/upload');
+const db = require('../knex/knex');
 const allFileDetails = async(req,res) => {
+    try 
+    {
+        const result = await db('file').select('file_name','file_serial');
+        res.json(result);
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).json("message: Error Occcured");
+    }
 
 }
 
@@ -8,14 +19,24 @@ const getFile = async(req,res) => {
 }
 
 const sendFile = async(req,res) => {
-    // multipart file data in body with title, also assign random string
     console.log("Reached Route: Send File");
     try {
         await uploadFile(req, res);
         if (req.file == undefined) {
           return res.status(400).send({ message: "Please upload a file!" });
         }
-    
+        try 
+        {
+            const results = await db('file').insert({
+                file_name: req.file.originalname,
+                file_serial: randomStringGenerator()
+            }).returning('*');
+        }
+        catch(err)
+        {
+            console.log("Database Error");
+            console.log(err);
+        }
         res.status(200).send({
           message: "Uploaded the file successfully: " + req.file.originalname,
         });
